0ecb212eb4ae885d05d61db87991a341e0c6a62f
Parthiv Mohan <parthiv.mohan@gmail.com>
2020-01-09 14:49:57 -0500

    reinitiation commit

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var leagueRouter = require('./routes/league');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 app.use('/league', leagueRouter);
 
 // catch 404 and forward to error handler
